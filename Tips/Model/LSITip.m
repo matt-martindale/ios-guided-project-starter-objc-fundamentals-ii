06aa3a16017c1269edf6c68c8c1d4e2299d278ad
Matt Martindale <martindalematt18@yahoo.com>
2020-07-23 19:44:23 -0600

    Added LSITip model file

---
+++ Tips/Model/LSITip.m
@@ -0,0 +1,13 @@
+//
+//  LSITip.m
+//  Tips
+//
+//  Created by Matthew Martindale on 7/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSITip.h"
+
+@implementation LSITip
+
+@end
