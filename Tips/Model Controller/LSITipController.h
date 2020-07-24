3826ecd7a14ab3036a62eac973946666cbe574ea
Matt Martindale <martindalematt18@yahoo.com>
2020-07-23 20:13:17 -0600

    Added model controller

---
+++ Tips/Model Controller/LSITipController.h
@@ -0,0 +1,17 @@
+//
+//  LSITipController.h
+//  Tips
+//
+//  Created by Matthew Martindale on 7/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSITipController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tips/Model Controller/LSITipController.m
@@ -0,0 +1,13 @@
+//
+//  LSITipController.m
+//  Tips
+//
+//  Created by Matthew Martindale on 7/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSITipController.h"
+
+@implementation LSITipController
+
+@end
